edc9481413b4041ed78d2633b4ad4df05a9be42e
vikernes1981 <kostaspapas321@gmail.com>
2024-11-06 13:47:51 +0100

    Changed cors

--- pet-adoption/pet-adoption-backend/server.js
+++ pet-adoption/pet-adoption-backend/server.js
@@ -17,9 +17,11 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(bodyParser.json());
 app.use(cors({
-  origin: 'https://pawsome-homes.vercel.app',
+  origin: [
+    'http://localhost:5173',  // Allow local development
+    'https://pawsome-homes.vercel.app'  // Allow production
+  ],
 }));
 
 // MongoDB connection
